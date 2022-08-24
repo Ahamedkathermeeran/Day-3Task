c0957ac59d2e2b44b1ac6cc81b2b32df48fb51e2
Ahamedkathermeeran <110997538+Ahamedkathermeeran@users.noreply.github.com>
2022-08-24 17:28:37 +0530

    Add files via upload

---
+++ Resume in JSON Format.js
@@ -0,0 +1,16 @@
+let myResume={
+    "basics": {
+      "name": "Ahamed kather meeeran",
+      "email": "katherupm001@gamil.com",
+      "phone": 9790244936,
+      "degree": "B.com",
+      "location": {
+        "address": "kallarai street,near postoffice,uthamapalayam,Theni Dt",
+        "postalCode": 625533,
+        "city": "Theni",
+        "state": "Tamilnadu",
+        "country": "India"
+    }
+     } }
+  console.log(myResume);
+  
---
+++ script.js
@@ -0,0 +1,27 @@
+// For loop
+for (var i = 0; i < 10; i++) {
+    console.log("AHAMED GUVI GEEK");
+  }
+// for in loop
+const string = "Geek for geek";
+for (let i in string) {
+    console.log(string[i]);
+}
+// for of loop
+const students = ['Ahamed', 'kather', 'meeran'];
+for ( let element of students ) 
+    console.log(element);
+// for each
+    const items = [12, 24, 36];
+    const copy = [];
+
+    items.forEach(function (item) {
+        copy.push(item + item+2);
+    });
+
+    document.write(copy);	
+
+					
+
+
+
